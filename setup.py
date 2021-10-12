b8ca918d664b4f4270b11b006bfd3bd094fb338f
levi <iooicoder@gmail.com>
2021-10-12 17:42:56 +0800

    feat: update google API

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-from setuptools import setup, find_packages
+import pathlib
+import pkg_resources
+
+from setuptools import find_packages, setup
+
+with pathlib.Path('requirements.txt').open() as fp:
+    install_requires = [
+        str(requirement) for requirement in pkg_resources.parse_requirements(fp)
+    ]
 
 setup(name='lunar_birthday',
-      version='1.0',
+      version='2.0',
       description='Lunar Birthday to Google Calendar',
       author='tuo',
-      author_email='iooicoder@gmail.com',
+      author_email='',
       url='https://github.com/tuot',
       packages=find_packages(),
       keywords=['google', 'lunar birthday', 'calendar', 'google calendar'],
-      install_requires=[
-          'google-api-python-client==1.7.8',
-          'oauth2client==4.1.3',
-          'zhdate==0.1',
-      ],
+      install_requires=install_requires,
       entry_points={
-          "console_scripts": [
-              "lb=lunar_birthday.cmd:main",
+          'console_scripts': [
+              'lb=lunar_birthday.command:main',
           ],
       }
       )
